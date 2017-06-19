fc3857731172d1a2bfd795a37e25e95d56c94a4b
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-19 15:21:28 -0500

    Create a Post class. This class will ultimately represent a post record from our database. The class should have private properties and getters and setters for a title and body.

---
+++ src/main/java/com/codeup/controllers/Post.java
@@ -0,0 +1,30 @@
+package com.codeup.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by canidmars on 6/19/17.
+ */
+
+@Controller
+public class Post {
+
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
