4baf76a9ea856b5219631560d91e073e313524db
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-22 16:05:10 -0500

    Relationship exercises
    Created a User class, with fields for id, username, email, and password.

--- src/main/java/com/codeup/models/User.java
+++ src/main/java/com/codeup/models/User.java
@@ -1,9 +1,27 @@
 package com.codeup.models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by canidmars on 6/22/17.
  */
 public class User {
 
     //ToDo: add properties, constructors, get/set
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
