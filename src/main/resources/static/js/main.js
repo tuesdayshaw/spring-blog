088016df3310d8f9bfd34b838cb7984126385dc2
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-28 10:28:51 -0500

    Merging files from json_exercises

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by canidmars on 6/23/17.
+ */
+
+(function ($) {
+    "use strict";
+
+    var request = $.ajax({
+        url: "/posts.json"
+    });
+
+    request.done(function (posts) {
+
+        var html = "", i;
+        var $posts = $("#index-posts-flexbox");
+
+        for (i = 0; i < posts.length; i++) {
+            html += "<div class='post-container'><h1>" + posts[i].title + "</h1><p>" + posts[i].body + "</p></div>";
+        }
+
+        $posts.append(html);
+    });
+
+})(jQuery);
